c81ba5aef7267200b15b3b28ab4dc80c25b51841
Steven Gallegos <steven.gallegos@uisek.edu.ec>
2025-10-27 23:47:41 -0500

    Finalización Laboratorio 4

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Pokemon, Entrenador
 
 # Register your models here.
+@admin.register(Pokemon)
+class PokemonAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Entrenador)
+class EntrenadorAdmin(admin.ModelAdmin):
+    pass
--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon (models.Model ):
+    name = models.CharField(max_length=30, null=False)
+    tipo = models.CharField(max_length=30, null=False)
+    weight = models.DecimalField(decimal_places=4, max_digits=6)
+    height = models.DecimalField(decimal_places=4, max_digits=6)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Entrenador(models.Model):
+    nombre = models.CharField(max_length=30, null=False)
+    apellido = models.CharField(max_length=30, null=False)
+    nivel = models.IntegerField(default=1)
+    fecha_nacimiento = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
