2f99087e061e55fc1a3a5f2f34df5a216a65501c
Steven Gallegos <steven.gallegos@uisek.edu.ec>
2025-12-09 22:06:41 -0500

    creación de aplicación para api

--- api/views.py
+++ api/views.py
@@ -1,9 +1,3 @@
-from rest_framework import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from pokedex.models import Pokemon
-from .serializers import PokemonSerializer
+from django.shortcuts import render
 
-class PokemonList(APIView):
-    def get(self, request):
-        pokemons = 
+# Create your views here.
