78e803d94951b6bfc11bb2a7609c4d6b027da08f
Steven Gallegos <steven.gallegos@uisek.edu.ec>
2025-12-14 23:43:29 -0500

    actualizacion oauth

--- api/views.py
+++ api/views.py
@@ -1,13 +1,31 @@
 from rest_framework import viewsets
 from pokedex.models import Pokemon, Entrenador
 from .serializers import PokemonSerializer, EntrenadorSerializer
+from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasScope
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
 
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
+    
+    authentication_classes = [OAuth2Authentication]
     required_scopes = ['write']
+    
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated(), TokenHasScope()]
+        return [AllowAny()]
     
 class EntrenadorViewSet(viewsets.ModelViewSet):
     queryset = Entrenador.objects.all()
     serializer_class = EntrenadorSerializer
+
+    authentication_classes = [OAuth2Authentication]
     required_scopes = ['write']
+
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated(), TokenHasScope()]
+        return [AllowAny()]
--- lab8/settings.py
+++ lab8/settings.py
@@ -38,8 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'oauth2_provider',
     'pokedex',
     'api',
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -50,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'oauth2_provider.middleware.OAuth2TokenMiddleware',
 ]
 
 ROOT_URLCONF = 'lab8.urls'
@@ -133,3 +137,20 @@ LOGIN_REDIRECT_URL="/"
 LOGOUT_REDIRECT_URL="/"
 LOGIN_URL ="pokedex:login"
 
+OAUTH2_PROVIDER = {
+}
+
+AUTHENTICATION_BACKENDS = (
+    'oauth2_provider.backends.OAuth2Backend',
+    'django.contrib.auth.backends.ModelBackend'
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
