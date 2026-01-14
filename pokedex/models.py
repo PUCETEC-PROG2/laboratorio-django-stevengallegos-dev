0f336775e7803bf33e7b239a625a1aee12f96025
Steven Gallegos <steven.gallegos@uisek.edu.ec>
2026-01-13 22:13:17 -0500

    actualizacion lab 11

--- pokedex/models.py
+++ pokedex/models.py
@@ -17,15 +17,16 @@ class Entrenador(models.Model):
 # Create your models here.
 class Pokemon (models.Model ):
     name = models.CharField(max_length=30, null=False)
-    POKEMON_TYPES = {
-        ('A', 'Agua'),
-        ('F', 'Fuego'),
-        ('T', 'Tierra'),
-        ('P', 'Planta'),
-        ('E', 'Eléctrico'),
-        ('L', 'Lagartija'),    
-    }
-    tipo = models.CharField(max_length=1, choices=POKEMON_TYPES, null=False)
+    POKEMON_TYPES = [
+    ('Agua', 'Agua'),
+    ('Fuego', 'Fuego'),
+    ('Tierra', 'Tierra'),
+    ('Planta', 'Planta'),
+    ('Eléctrico', 'Eléctrico'),
+    ('Lagartija', 'Lagartija'),
+]
+
+    tipo = models.CharField(max_length=15, choices=POKEMON_TYPES, null=False)
     weight = models.DecimalField(decimal_places=4, max_digits=6)
     height = models.DecimalField(decimal_places=4, max_digits=6)
     picture = models.ImageField(upload_to='pokemons', null=True, blank=True)
